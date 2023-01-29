a37af7cb14aee344345e7d187a6f6bfbc3b517d3
jderez <jderez@mcad.edu>
2023-01-29 22:10:46 +0100

    update exercises

---
+++ week 3/exercises/functions/main.js
@@ -0,0 +1,61 @@
+logOutGreeting('yo');
+
+function logOutGreeting(greeting) {
+    console.log(greeting);
+}
+logOutGreeting('Salutations');
+
+// function to get stuff
+function getStuff(ingredient) {
+    console.log('get ' + ingredient);
+}
+
+getStuff('bread');
+getStuff('peanut butter');
+getStuff('honey');
+
+// example of an anonymous function
+let makeSandwich = (ingredientArray) => { 
+    for (let i = 0; i < ingredientArray.length; i++) 
+    {
+    console.log('get ', ingredientArray[i]);
+    }
+};
+
+makeSandwich = (ingredientArray) => {
+    console.log(ingredientArray.toString(', '))
+};
+
+makeSandwich(['bread', 'mayo', 'tunafish']);
+makeSandwich(['this', 'that', 'thing']);
+
+
+const output = 12;
+function makeRand (maxNumber) {
+    const output = Math.ceil(Math.random() * maxNumber);
+    return output;
+}
+const randomOutput = makeRand(100);
+console.log(randomOutput);
+
+console.log(output);
+function checkLegality (age) {
+    return age >= 21;
+}
+
+const KensAge = 49; 
+if (checkLegality(KensAge)) {
+    console.log('He is legal'); 
+}
+
+
+const myObject = {
+    x: 200,
+    y: 400,
+    getPosition: () => {
+        this.x = 35;
+        this.y = 45;
+       console.log('we are ' + this.x + ' from the left and ' + this.y + ' form the top');
+    }
+}
+myObject.getPosition();
---
+++ week 3/exercises/loops/main.js
@@ -0,0 +1,27 @@
+// for loop with number
+for (let i = 0; i < 10; i++) {
+   console.log('this is i', i);
+} 
+
+// for loop with an array
+const myMovies = ['star wars', 'empire strikes back', 'return of the jedi', 'raiders of the lost ark', 'last crusade'];
+for (let j = 0; j < myMovies.length; j++) {
+    console.log('i like', myMovies[j]);
+}
+
+// while loop -- a little scary but not bad
+let myVar = false;
+while (myVar === false) {
+    console.log('this will happen once');
+    myVar = true; 
+}
+
+// while loop removing items in an array
+const myNums = [1, 2, 3, 4, 5, 6];
+while (myNums.length > 0) {
+    console.log('removing ' + myNums[myNums.length-1]);
+    myNums.pop();
+}
+console.log(myNums);
+
+// forEach
