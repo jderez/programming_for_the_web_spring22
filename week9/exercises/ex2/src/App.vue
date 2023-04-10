172409afbc11a6ec5df45d103ad5bd0834b2feb3
jderez <jderez@mcad.edu>
2023-04-10 13:06:35 +0200

    update

---
+++ week9/exercises/ex2/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+// javascript here
+import CamelidRow from "./components/CamelidRow.vue";
+const camelids = [
+      {
+        commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
+      },
+    ]
+</script>
+
+
+<template>
+<!-- html goes here -->  
+<h1>Camelids</h1>
+ <table>
+    <thead>
+      <th>Common Name</th>
+      <th>Binomial Name</th>
+      <th>Order</th>
+      <th>Family</th>
+      <th>Genus</th>
+      <th>Species</th>
+      <th>Image</th>
+    </thead>
+    <tbody>
+      <CamelidRow v-for="camelid in camelids" v-bind:camelid-name="camelid.commonName" v-bind:camelid-binomial="camelid.binomialName" v-bind:camelid-order="camelid.order" v-bind:camelid-family="camelid.family" v-bind:camelid-genus="camelid.genus" v-bind:camelid-species="camelid.species" v-bind:camelid-image="camelid.image" v-bind:key="camelid.commonName">
+      </CamelidRow>
+    </tbody>
+ </table>
+</template>
+
+
+<style>
+/* style goes here */
+
+</style>
---
+++ week9/exercises/ex2/src/components/CamelidRow.vue
@@ -0,0 +1,39 @@
+<script setup>
+    const props = defineProps(["camelidName", "camelidBinomial", "camelidOrder", "camelidFamily", "camelidGenus", "camelidSpecies", "camelidImage"])
+</script>
+
+<template>
+    <tr>
+        <td>{{ props.camelidName }}</td>
+        <td>{{ props.camelidBinomial }}</td>
+        <td>{{ props.camelidOrder }}</td>
+        <td>{{ props.camelidFamily }}</td>
+        <td>{{ props.camelidGenus }}</td>
+        <td>{{ props.camelidSpecies }}</td>
+        <td><img class="camelid-img" v-bind:src="camelidImage"></td>
+      </tr>
+</template>
+
+<style scoped>
+.camelid-img {
+    max-width: 125px;
+}
+
+* {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+tr {
+    text-align: center;
+}
+
+td {
+    padding: 30px;
+    border: 1px solid black;
+    border-radius: 10px;
+}
+
+img {
+    max-width: 100px;
+}
+</style>
