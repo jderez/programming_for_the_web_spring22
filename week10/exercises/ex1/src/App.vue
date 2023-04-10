88113217107e12da299fe08c6dea6a420c26b813
jderez <jderez@mcad.edu>
2023-04-10 19:48:28 +0200

    update

---
+++ week10/exercises/ex1/src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> | 
+    <RouterLink to="/alpaca">Alpaca</RouterLink> | 
+    <RouterLink to="/llama">Llama</RouterLink> 
+  </nav>
+  <div class="container">
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<style scoped>
+html {
+  font-size: 16px;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+
+body, 
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  width: 100%;
+  height: 45px;
+  background: #0e0e3f;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+nav a, nav a:hover, nav a:link, nav a:active, nav a:visited {
+  color: white;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 1.75;
+  text-decoration: none;
+}
+
+nav a.router-link-active {
+  text-decoration: underline;
+  color: rgb(234, 234, 150);
+}
+
+.container {
+  max-width: 1200px;
+  width: 100%;
+  margin: 20px auto 20px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.container h3 {
+  margin: 20px auto 20px;
+}
+
+.container h1 {
+  text-align: center;
+  font-size: 48px;
+  margin: 20px auto 20px;
+}
+
+.container p {
+  width: 1000px;
+  font-size: 14px;
+  font-size: inherit;
+  margin: 20px auto 20px;
+}
+
+.container img {
+  display: block;
+  max-width: 1000px;
+  width: 100%;
+  margin: 20px auto 20px;
+}
+</style>
---
+++ week10/exercises/ex1/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1>Which are better, llamas or alpacas?</h1>
+        <p>
+         Is this the last word in the debate? Have you made a choice? Use the
+        navigation to learn more about each one and then you can decide which win:
+    <strong>Llamas</strong> or <strong>Alpacas</strong>
+        </p>
+</template>
+
+<style scoped>
+html {
+  font-size: 16px;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+
+body, 
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  width: 100%;
+  height: 45px;
+  background: #0e0e3f;
+}
+
+nav a, nav a:hover, nav a:link, nav a:active, nav a:visited {
+  color: white;
+  font-weight: 900;
+  font-size: 24px;
+}
+.container {
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.container h1 {
+  text-align: center;
+  font-size: 48px;
+}
+
+.container p {
+  width: 1000px;
+  font-size: 14px;
+  font-size: inherit;
+  margin: 20px auto 20px;
+}
+
+.container img {
+  display: block;
+  max-width: 1000px;
+  width: 100%;
+  margin: 20px auto 20px;
+}
+</style>
