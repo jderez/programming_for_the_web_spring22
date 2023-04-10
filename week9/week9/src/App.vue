c1ba8cbf45a432a2bff07cdceb497dfe33aeb19c
jderez <jderez@mcad.edu>
2023-04-10 11:08:54 +0200

    update

---
+++ week9/week9/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// javascript here
+const isWorking = true;
+</script>
+
+
+<template>
+<!-- html goes here -->  
+<div class="container">
+  <h1>My first compile Vue app</h1>
+  <p v-if="isWorking">It is working</p>
+</div>
+</template>
+
+
+<style>
+/* style goes here */
+  .container {
+    width: 600px;
+    margin: 20px auto 0;
+    text-align: center;
+    font-size: 18px;
+  }
+
+  .container p {
+    font-size: 18px;
+  }
+</style>
